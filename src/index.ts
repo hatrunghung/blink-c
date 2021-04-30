fb64c28b3fca1167b1a9f2c0076a8f64b450be76
Hung Ha Trung <hungtro96@gmail.com>
2021-05-01 02:12:11 +1000

    add button component to entry point

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 import TestComponent from './TestComponent/TestComponent';
 
 export { TestComponent };
+
+export {
+  AnchorButton,
+  Button,
+  ButtonGroup,
+  IconButton,
+  SplitButton,
+  ToggleButton,
+  ToggleIconButton,
+} from './Button';
+export type {
+  IAnchorButtonProps,
+  IButtonProps,
+  IIconProps,
+  IButtonGroupProps,
+  IIconButtonProps,
+  IToggleButtonProps,
+  IToggleIconButtonProps,
+} from './Button';
