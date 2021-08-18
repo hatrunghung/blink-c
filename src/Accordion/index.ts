e5cca01c3a8021ad25cda9b367b8acd6015798d7
Hung Ha Trung <hungtro96@gmail.com>
2021-08-19 01:49:15 +1000

    fix entry point for Accordion

--- src/Accordion/index.ts
+++ src/Accordion/index.ts
@@ -1,11 +1,4 @@
-export { Accordion } from './Accordion';
+export { default as Accordion } from './Accordion';
 export type { IAccordionProps } from './Accordion';
 
-export { AccordionSection } from './AccordionSection';
-
-export { AccordionHeader } from './AccordionHeader';
 export type { IAccordionHeaderProps } from './AccordionHeader';
-
-export { AccordionLabel } from './AccordionLabel';
-
-export { AccordionPanel } from './AccordionPanel';
