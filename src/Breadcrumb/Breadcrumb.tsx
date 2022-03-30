90ccdd87070e56663933d9dafa9a283198678ae3
Hung Ha Trung <hungtro96@gmail.com>
2022-03-30 18:29:21 +1100

    change <Breadcrumb /> type

--- src/Breadcrumb/Breadcrumb.tsx
+++ src/Breadcrumb/Breadcrumb.tsx
@@ -9,32 +9,32 @@ import { useBreadcrumb } from '../hooks';
 import { StyledBreadcrumb } from './StyledBreadcrumb';
 import { StyledBreadcrumbItem } from './StyledBreadcrumbItem';
 
-export const Breadcrumb: FunctionComponent<RefAttributes<any>> = forwardRef(
-  (props, ref) => {
-    const { containerProps, currentPageProps } = useBreadcrumb();
+export const Breadcrumb: FunctionComponent<
+  RefAttributes<HTMLElement>
+> = forwardRef((props, ref) => {
+  const { containerProps, currentPageProps } = useBreadcrumb();
 
-    const childrenCount = Children.count(props.children);
+  const childrenCount = Children.count(props.children);
 
-    const childrenMapped = Children.map(props.children, (child, index) => {
-      const isLastItem = index === childrenCount - 1;
+  const childrenMapped = Children.map(props.children, (child, index) => {
+    const isLastItem = index === childrenCount - 1;
 
-      if (isLastItem) {
-        return (
-          <StyledBreadcrumbItem>
-            {cloneElement(child as any, currentPageProps)}
-          </StyledBreadcrumbItem>
-        );
-      }
+    if (isLastItem) {
+      return (
+        <StyledBreadcrumbItem>
+          {cloneElement(child as any, currentPageProps)}
+        </StyledBreadcrumbItem>
+      );
+    }
 
-      return <StyledBreadcrumbItem>{child}</StyledBreadcrumbItem>;
-    });
+    return <StyledBreadcrumbItem>{child}</StyledBreadcrumbItem>;
+  });
 
-    return (
-      <nav ref={ref} {...containerProps}>
-        <StyledBreadcrumb>{childrenMapped}</StyledBreadcrumb>
-      </nav>
-    );
-  },
-);
+  return (
+    <nav ref={ref} {...containerProps}>
+      <StyledBreadcrumb>{childrenMapped}</StyledBreadcrumb>
+    </nav>
+  );
+});
 
 Breadcrumb.displayName = 'Breadcrumb';
