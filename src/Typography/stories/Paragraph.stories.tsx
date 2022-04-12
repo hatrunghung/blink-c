5aad7024234ab5026026c706361664edf1ee1f5d
Hung Ha Trung <hungtro96@gmail.com>
2022-04-13 01:24:02 +1000

    change paragraph stories content

--- src/Typography/stories/Paragraph.stories.tsx
+++ src/Typography/stories/Paragraph.stories.tsx
@@ -14,13 +14,25 @@ export const Default: Story = ({ size, isBold }) => {
       <Row>
         <Col>
           <Paragraph size={size} isBold={isBold}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Jisoo was born on January 3, 1995 in Gunpo. She&rsquo;s a member of
+            Blackpink. She became a YG Entertainment trainee in August 2011
+            &amp; trained for 5 years. In August 2016, she became a member of
+            Blackpink, as a lead vocalist &amp; visualist in the group. Before
+            she became a trainee at YG, she was known by many for her beauty.
+            She was very popular in school because of her beautiful face and
+            talents. She used to study at the School Of Performing Arts High
+            School. She eventually transferred schools when her family moved to
+            Seoul, South Korea. In 2012, she was revealed through the
+            &ldquo;Who&rsquo;s That Girl?&rdquo; teaser by YG Entertainment, and
+            it was followed with two photos in January of the next year. On
+            August 18, 2020, YG Entertainment announced that Jisoo will be
+            making her acting debut. She will be debuting as the lead role in
+            the upcoming JTBC drama, Snowdrop. The drama is expected to air
+            sometime in 2021. On December 30, 2020, it was announced that Jisoo
+            will be having her solo debut when the filming for her drama,
+            Snowdrop is finished. On August 18, a video was posted onto JTBC
+            Drama&rsquo;s YouTube channel announcing Snowdrop as part of the
+            upcoming Drama Line-up.
           </Paragraph>
         </Col>
       </Row>
